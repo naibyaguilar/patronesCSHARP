0061e1af495d2fb17e73ff96b1203f7a6ac4335e
MARIOBALLINA <94090819+MARIOBALLINA@users.noreply.github.com>
2021-11-21 19:58:59 -0600

    Add files via upload

---
+++ Patron_MARIO_prototype/Patrones.Prototype/Patrones.Prototype.Core/AlfaRomeoPrototipo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Prototype.Core
+{
+    public class AlfaRomeoPrototipo : AutoPrototipo
+    {
+        public override AutoPrototipo Clonar()
+        {
+            return (AlfaRomeoPrototipo)this.MemberwiseClone();
+        }
+
+        public override string VerAuto()
+        {
+            return $"Alfa Romeo {_modelo} color {_color}";
+        }
+    }
+}
---
+++ Patron_MARIO_prototype/Patrones.Prototype/Patrones.Prototype.Core/DSPrototipo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrones.Prototype.Core
+{
+    public class DSPrototipo : AutoPrototipo
+    {
+        public override AutoPrototipo Clonar()
+        {
+            return (DSPrototipo)this.MemberwiseClone();
+        }
+        public override string VerAuto()
+        {
+            return $"DS {_modelo} color {_color}";
+        }
+    }
+}
