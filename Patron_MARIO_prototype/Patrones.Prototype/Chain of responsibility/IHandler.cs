0a6e5c4f3046609476d51c392d456a546eb84383
NAIBY CECILIA AGUILAR MORALES <naibyaguilar@hotmail.com>
2021-11-23 22:50:57 -0600

    Fist Behavioral Design Patterns - Chain of Responsibility

---
+++ Patron_MARIO_prototype/Patrones.Prototype/Chain of responsibility/IHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain_of_responsibility
+{
+     // The Handler interface declares a method for building the chain of
+    // handlers. It also declares a method for executing a request.
+    public interface IHandler
+    {
+       IHandler SetNext(IHandler handler);
+
+    object Handle(object request);
+}
+}
