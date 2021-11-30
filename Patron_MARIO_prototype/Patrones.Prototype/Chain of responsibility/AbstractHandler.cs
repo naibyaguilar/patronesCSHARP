75d272251d0275ff85b3d5ff9f8f722da640b3ac
NAIBY CECILIA AGUILAR MORALES <naibyaguilar@hotmail.com>
2021-11-30 08:06:54 -0600

    Pratrones varios

---
+++ Patron_MARIO_prototype/Patrones.Prototype/Chain of responsibility/AbstractHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chain_of_responsibility
+{
+    //  El comportamiento de encadenamiento por defecto puede ser implementado dentro de un handler base
+    abstract class AbstractHandler :IHandler
+    {
+    private IHandler _nextHandler;
+
+    public IHandler SetNext(IHandler handler)
+    {
+        this._nextHandler = handler;
+
+        // Returning a handler from here will let us link handlers in a
+        // convenient way like this:
+        // monkey.SetNext(squirrel).SetNext(dog);
+        return handler;
+    }
+
+    public virtual object Handle(object request)
+    {
+        if (this._nextHandler != null)
+        {
+            return this._nextHandler.Handle(request);
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+}
